2b49e123d38e2bc1468eb98fe8478362ad04507a
Alejandro Alvarez <a.alejandro.alvaerz.a@gmail.com>
2022-10-24 17:28:53 +0200

    feature(global): add section to connect with arduino

--- index.py
+++ index.py
@@ -1,6 +1,15 @@
+
+
+
+
 # Use Tkinter for python 2, tkinter for python 3
+from tkinter import Tk, RIGHT, BOTH, RAISED, LEFT
 from tkinter import Tk, LabelFrame, Label, StringVar, W
-from tkinter import Frame, OptionMenu
+from tkinter import Frame, OptionMenu, Button
+
+import sys
+import glob
+import serial
 
 class ConectionPort(Frame):
     def __init__(self, parent = None):
@@ -10,43 +19,97 @@ class ConectionPort(Frame):
 
     def createWidgets(self):
         self.connectionContainer = LabelFrame(self)
-        self.connectionContainer["text"] = "container"
+        self.connectionContainer["text"] = " Container "
         self.connectionContainer.grid(row = 0, column = 0, columnspan = 3, pady = 10, padx = 10)
 
-        self.portNameLabel = Label(self.connectionContainer)
-        self.portNameLabel["text"] = "Port name label:"
-        self.portNameLabel.grid(column = 1, row = 1)
-
         # initialize data
-        self.languages = ('Python', 'JavaScript', 'Java',
-                        'Swift', 'GoLang', 'C#', 'C++', 'Scala')
+        
+        self.selectPort()
+        
 
-        # set up variable
-        self.option_var = StringVar(self.connectionContainer)
+    def selectPort(self):
+        paddings = {'padx': 15, 'pady': 10}
+        self.languages = self.connectPorts() or ['empty']
+        self.rate = ['9600', '10000', '1200']
 
-        # padding for widgets using the grid layout
-        paddings = {'padx': 25, 'pady': 25}
+        # portNameLabel
+        self.portNameLabel = Label(self.connectionContainer)
+        self.portNameLabel["text"] = "Port name:"
+        self.portNameLabel.grid(row = 0, column = 0)
+        self.portName = StringVar(self.connectionContainer)
 
-        # label
-        self.label = Label(self.connectionContainer,  text='Select your most favorite language:')
-        self.label.grid(column=0, row=0, sticky=W, **paddings)
-
-        # option menu
-        self.option_menu = OptionMenu(
+        # selectPortName
+        self.selectPortName = OptionMenu(
             self.connectionContainer,
-            self.option_var,
-            self.languages[0],
+            self.portName,
             *self.languages,
-            command=self.option_changed)
+            command=self.onSelectPortName)
+        self.selectPortName.config(width = 10)
+        self.selectPortName.config(bg='light blue')
+        self.selectPortName.grid(row = 0, column = 1, sticky=W, **paddings)
 
-        self.option_menu.grid(column=10, row=10, sticky=W, **paddings)
+        # bauteRateLabel
+        self.bauteRateLabel = Label(self.connectionContainer)
+        self.bauteRateLabel["text"] = "Baoud Rate:"
+        self.bauteRateLabel.grid(row = 2, column = 0)
+        self.bauteRate = StringVar(self.connectionContainer)
 
-        # output label
-        self.output_label = Label(self.connectionContainer, foreground='red')
-        self.output_label.grid(column=0, row=1, sticky=W, **paddings)
+        # selectBauteRate
+        self.selectBauteRate = OptionMenu(
+            self.connectionContainer,
+            self.bauteRate,
+            *self.rate,
+            command=self.onSelectBauteRate)
+        self.selectBauteRate.config(width = 10)
+        self.selectBauteRate.config(bg='green')
+        self.selectBauteRate.grid(row = 2, column = 1, sticky=W, **paddings)
 
-    def option_changed(self, *args):
-        self.output_label['text'] = f'You selected: {self.option_var.get()}'
+        # connect
+        self.outputPortNameLabel = Label(self.connectionContainer, foreground='red')
+        self.outputPortNameLabel.grid(row = 1, column = 0, sticky = W, **paddings)
+
+
+    def onSelectPortName(self, *args):
+        self.outputPortNameLabel['text'] = f'You selected: {self.portName.get()}'
+
+    def onSelectBauteRate(self, *args):
+        print(self.bauteRate.get())
+
+        
+    def connectPorts(self):
+        """ Lists serial port names
+            :raises EnvironmentError:
+                On unsupported or unknown platforms
+            :returns:
+                A list of the serial ports available on the system
+        """
+        if sys.platform.startswith('win'):
+            ports = ['COM%s' % (i + 1) for i in range(256)]
+        elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+            # this excludes your current terminal "/dev/tty"
+            ports = glob.glob('/dev/tty[A-Za-z]*')
+        elif sys.platform.startswith('darwin'):
+            ports = glob.glob('/dev/tty.*')
+        else:
+            raise EnvironmentError('Unsupported platform')
+
+        result = []
+        for port in ports:
+            try:
+                s = serial.Serial(port)
+                s.close()
+                result.append(port)
+            except (OSError, serial.SerialException):
+                pass
+        return result
+
+    def showDropDown(self):
+        pass
+    
+    def showEmptyConnect(self):
+        pass
+
+
         
 class GUI(Frame):
     def __init__(self, parent, *args, **kwargs):
@@ -56,7 +119,7 @@ class GUI(Frame):
         self.parent.title('Software to scanner')
 
         self.connectionPort = ConectionPort(self)
-        self.connectionPort.pack()
+        self.connectionPort.pack(side = LEFT)
 
 
 if __name__ == "__main__":
@@ -64,4 +127,6 @@ if __name__ == "__main__":
     GUI(root).pack(side="top", fill="both", expand=True)
     root.mainloop()
 
+
 # https://stackoverflow.com/questions/17466561/best-way-to-structure-a-tkinter-application
+
