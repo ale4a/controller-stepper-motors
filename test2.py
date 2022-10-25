d2ca1e15afa32b4b8216693380ffcb313a42225d
AlejandroAlvarezA4 <a.alejandro.alvaerz.a@gmail.com>
2022-10-25 17:27:58 +0200

    chore(test2): add spaces

--- test2.py
+++ test2.py
@@ -1,7 +1,9 @@
 from tkinter import *
 from tkinter import ttk
+
 def func():
     l2.configure(text=cb.get())
+    
 win =Tk()
 win.geometry('200x100')
 course=["Java","Python","C & C++"]
@@ -11,6 +13,8 @@ cb=ttk.Combobox(win,values=course,width=10)
 cb.grid(column=0, row=1)
 cb.current(0)
 print(cb.get())
+
+
 b=Button(win,text="Click Here",command=func)
 b.grid(column=0, row=2)
 l2=Label(win,text="")
